80b03f1fa1e0811cbfe7f035e079e5068310e02d
Airjt <905270094@qq.com>
2019-03-12 09:57:21 +0800

    3.11

--- S/js/index.js
+++ S/js/index.js
@@ -1,6 +1,9 @@
 var swiper = new Swiper('.swiper-container',{
+  autoplay:false,
+  direction: 'vertical',
     pagination: {
         el: '.swiper-pagination',
         clickable:true,
       },
+      
     });
