bffb6ac8ed2074806ea5ca521111f956d8e4e8e8
Airjt <905270094@qq.com>
2019-03-13 22:54:13 +0800

    3.13 11

--- S/js/index.js
+++ S/js/index.js
@@ -1,4 +1,7 @@
 var swiper = new Swiper('.swiper-container',{
+  hashNavigation: {
+    watchState: true,
+  },
   autoplay:false,
   direction: 'vertical',
     pagination: {
@@ -11,7 +14,18 @@ $(document).ready(function(){
   $(".openbtn").click(function(){
     $(".navbar").animate({width:'toggle'},350);
   });
+  $(".daohanglan a").click(function(){
+    $(".navbar").hide();
+  })
   // $(".qq").mouseover(function(){
   //   $(".qqer").fadeToggle();
   // })
+  for(var i = 0 ; i < 4 ; i++){
+    add(i);
+  }
+  function add(i){
+    $(".btn"+i).click(function(){
+      swiper.slideTo(i,1000,false)
+   })
+  }
 })
